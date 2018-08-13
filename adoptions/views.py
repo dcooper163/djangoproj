9425863db72a94b2de1ba53e6ba36abc109718cc
reelrebels <dcooper163@gmail.com>
2018-08-13 02:58:42 +0000

    added views

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
+
 
 def home(request):
-    return HttpResponse('<p>home view</p>')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets})
+    #return HttpResponse('<!doctype html><html lang="en"><head><meta charset="utf-8"><title>The HTML5 Herald</title><meta name="description" content="The HTML5 Herald"><meta name="author" content="SitePoint"><link rel="stylesheet" href="css/styles.css?v=1.0"></head><body><p>This is the home page</p><script src="js/scripts.js"></script></body></html>')
     
 def pet_detail(request, id):
-    return HttpResponse('<p>pet_detail view with the id {}</p>'.format(id))
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet Not Found')
+    return render(request, 'pet_detail.html', {'pet': pet})
+    
+    #return HttpResponse('<p>pet_detail view with the id {}</p>'.format(id))
